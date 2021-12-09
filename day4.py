eaadd213803565a84c9c76509e808cc9f2bdc173
24Chromosones <fakeemail1441@gmail.com>
2021-12-08 17:20:04 -0800

    finally finished d4 p1

--- day4.py
+++ day4.py
@@ -1,3 +1,6 @@
+#  We dont talk about day 4
+# This is the worst code ive ever written but i finally got the answer
+
 def part1():
     with open('inputs/day4.txt', 'r') as f:
         d4 = f.read().split('\n\n')
@@ -8,29 +11,46 @@ def part1():
     for i in range(1, len(d4)):
         games[f'game{i}'] = d4[i].split('\n')
         games[f'game{i}'] = [line for line in games[f'game{i}'] if line.strip() != '']
-        # for j in range(len(games[f'game{i}'])):
-        #     print(games[f'game{i}'][j])
-        # print('\n')
     first = True
     for i in numbers:
         for game in games:
             for row in range(len(games[game])):
-                # print(games[game][row])
-                # print(row)
                 if first:
                     games[game][row] = games[game][row].split()
-                    first = False
                 games[game][row] = ['-1' if x == i else x for x in games[game][row]]
-                if all(games[game][row]):
+                equal_number = games[game][row][0]
+                check = True
+                for o in games[game][row]:
+                    if equal_number != o:
+                        check = False
+                        break
+                if check:
+                    print(games[game][row] + 'a')
                     answer = 0
-                    for j in game:
+                    for j in games[game]:
                         for num in j:
                             if num != '-1':
                                 answer += int(num)
-                                return answer
-        for k in range(len(game[]))
+                    return answer * int(i)
+            for j in range(len(games[game])):
+                column = []
+                for k in games[game]:
+                    column.append(k[j])
+                equal_number = games[game][row][0]
+                check = True
+                for o in column:
+                    if equal_number != o:
+                        check = False
+                        break
+                if check:
+                    answer = 0
+                    for v in games[game]:
+                        for num in v:
+                            if num != '-1':
 
-            
+                                answer += int(num)
+                    return answer * int(i)
+        first = False
 
 
-part1()
+print(part1())
